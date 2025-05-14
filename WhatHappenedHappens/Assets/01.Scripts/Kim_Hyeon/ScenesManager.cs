b6e28873f2ac8f95afb9c751cda68c187b28f9e0
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-14 17:02:12 +0900

    # 금일 pull 및 UI 폴더 관리 작업
    # 최신 상태로 pull 받고,  UI용 폴더 관리 작업 들어갔습니다.
    # Script , Scenes , UI 폴더에 추가됬습니다.

---
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/ScenesManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Script.Scenes
+{
+    public class ScenesManager: MonoBehaviour
+    {
+        [SerializeField] private string mainSceneName = "MainScene";
+        [SerializeField] private string uiSceneName = "UIScene";
+
+        private void Start()
+        {
+            LoadSceneAdditively(mainSceneName);
+            LoadSceneAdditively(uiSceneName);
+        }
+
+        private void LoadSceneAdditively(string sceneName)
+        {
+            if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            {
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            }
+        }
+        
+    }
+}
