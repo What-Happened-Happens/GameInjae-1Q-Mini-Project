43e559142f91e529b7b6acb6635ff9bb60f046ba
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-14 21:15:59 +0900

    #Add : UIManager
    # UI 를 관리할 매니저 스크립트 생성
    # Delete : 이해가 부족했던 스크립트 삭제
    # 카드 키를 먹었을 때 카드키의 활성화 / 비활성화 관리 함수 생성

---
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/UIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // 추후, 모든 주석은 영어로 변경 
+    [SerializeField] GameObject uiPrefab;  // 생성 관리할 UI Prefab    
+    public bool IsGetCardKey { get; set; } = false; 
+
+    void Start()
+    {
+        uiPrefab.SetActive(false);        // 시작 할 때에는 uiPrefab을 비활성화로 시작. 
+    }
+
+    // 플레이어가 카드키 오브젝트를 먹었을 때 호출할 SetActive를 true로 변환★
+    void PlayerGetCardKey(bool isGetCardKey)
+    {
+        if (isGetCardKey == false) return;
+        Debug.Assert(isGetCardKey != true, $"카드키를 먹은 isGetCardKey 결과 값 : {isGetCardKey}");
+
+        if(isGetCardKey) // 카드키를 먹었을 때, 
+        {
+            uiPrefab.SetActive(true); // uiPrefab을 활성화 
+        }
+    }
+    
+
+
+}
