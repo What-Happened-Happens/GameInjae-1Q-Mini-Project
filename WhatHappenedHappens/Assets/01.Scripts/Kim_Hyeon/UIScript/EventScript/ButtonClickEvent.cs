6e7076c7a6bac6d45ce1bc34cff18d16488ea806
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-15 14:47:52 +0900

    #Add : Button Click Event Script 오브젝트 적용

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/EventScript/ButtonClickEvent.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/EventScript/ButtonClickEvent.cs
@@ -1,17 +1,14 @@
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-
 using UnityEngine;
-
-
-
 public class ButtonClickEvent : MonoBehaviour
 {  
-     
     public void OnButtonClicked()// 버튼 클릭 시에 들어갈 이벤트 
     {
         Debug.Log($"[{gameObject.name}] 버튼 클릭!");
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        
     }
 }
 
