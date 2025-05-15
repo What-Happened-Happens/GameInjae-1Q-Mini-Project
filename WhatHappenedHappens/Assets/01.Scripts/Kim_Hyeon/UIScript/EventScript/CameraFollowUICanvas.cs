ae6cb9c97f202b56efe918e905527a181ac15833
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-15 22:21:05 +0900

    #Fead : GetCardKey Hide / Show 코드 제작
    # delete : CanvasActiveManager : 불필요 스크립트 삭제

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/EventScript/CameraFollowUICanvas.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/EventScript/CameraFollowUICanvas.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class CameraFollowUICanvas : MonoBehaviour
 {
-    [SerializeField] private Camera _targetCamera;    // 따라 움직일 mainCamera 
+    [SerializeField] private Camera _targetCamera;      // 따라 움직일 mainCamera 
     [SerializeField] private Canvas _uiCanvas;
-    private Vector3 _localOffset;  // 카메라에 따라갈 UI canvas 위치 
+    private Vector3 _localOffset;                       // 카메라에 따라갈 UI canvas 위치 
 
     private void Awake()
     {        
         _uiCanvas = GetComponent<Canvas>();
         _uiCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-       
-        _localOffset = Vector2.zero;
+        Debug.Log($"_uiCanvas 렌더 모드 ScreenSpaceCamera로 지정");
+
+        _localOffset = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f));
+        Debug.Log($"_uiCanvas 가 카메라를 따라갈 Canvas 위치");
 
         if (_targetCamera == null)
             _targetCamera = GetComponent<Camera>();
