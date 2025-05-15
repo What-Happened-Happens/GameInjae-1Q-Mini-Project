3217a5492da11469d7ee84feb66d945ede3ae71a
chogangjin <g0508j@naver.com>
2025-05-15 22:40:07 +0900

    💚 Add |  클래스 생성
    - 각 기믹 오브젝트의 트리거들을 상속하는 부모클래스 TrueFalse 생성
    - 트리거(차단기)생성

---
+++ WhatHappenedHappens/Assets/01.Scripts/KangJin/CircuitBreaker.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CircuitBreaker : TrueFalse
+{
+    // Start is called before the first frame update
+    float elapsedTime;
+    public float maxTime;
+    bool isColliding = false;
+    void Start()
+    {
+        elapsedTime = 0f;
+        maxTime = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isColliding)
+        {
+            if (!isTrue)
+            {
+                if (Input.GetKey(KeyCode.B))
+                {
+                    Debug.Log("CircuitBreaker on!");
+                    isTrue = true;
+                    elapsedTime = 0f;
+                }
+            }
+        }
+        if (isTrue)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= maxTime)
+            {
+                Debug.Log("CircuitBreaker off!");
+                isTrue = false;
+                elapsedTime = 0f;
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameObject.FindWithTag("Player"))
+        {
+            if (!isColliding)
+            {
+                isColliding = true;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isColliding = false;
+    }
+}
---
+++ WhatHappenedHappens/Assets/01.Scripts/KangJin/TrueFalse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrueFalse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected bool isTrue = false;
+    public bool IsTrue { get { return isTrue; } }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
