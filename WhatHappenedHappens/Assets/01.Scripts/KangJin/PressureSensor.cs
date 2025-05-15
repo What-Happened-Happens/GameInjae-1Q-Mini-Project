c15ed51cd7a7997d9bffd1e3ad89a6058eaa4800
chogangjin <g0508j@naver.com>
2025-05-16 00:25:12 +0900

    💚 Add |  트리거 프리팹 생성
    - 트리거 프리팹 생성
    - 감압센서 코드 추가

---
+++ WhatHappenedHappens/Assets/01.Scripts/KangJin/PressureSensor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PressureSensor : TrueFalse
+{
+    // Start is called before the first frame update
+    float elapsedTime;
+    public float limitTime;
+    bool timeCheck = false;
+    void Start()
+    {
+        elapsedTime = 0;
+        limitTime = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeCheck)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+        if(elapsedTime >=limitTime)
+        {
+            isTrue = false;
+            timeCheck = false;
+            elapsedTime = 0;
+            Debug.Log("Sensor off!");
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isTrue = true;
+        elapsedTime = 0;
+        Debug.Log("Keep Pressing!");
+        Debug.Log(gameObject.transform.up);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        timeCheck = true;
+        Debug.Log("Pressured off!");
+    }
+}
