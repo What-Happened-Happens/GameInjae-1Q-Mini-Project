2c0b66df6be609e5f05cc0aedf5d2ee73088edb1
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-16 21:35:42 +0900

    # Sound Poolling Sript 생성
    # SoundSliderManager 파일 폴더 이동

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/SoundManager.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/SoundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,9 +10,10 @@ enum eSound
     shoot, 
     hit 
 }
+
 public class SoundManager : MonoBehaviour
 {
-    
+    const string SoundPath = "../Sounds"; 
     private AudioSource BGMAudioSource;
 
     [Header("AudioObject And AudioClip List")]
@@ -19,45 +21,54 @@ public class SoundManager : MonoBehaviour
     public List<AudioClip> SFXAudioClip = new List<AudioClip>();
 
     [Header("SoundSliders")]
-    public Slider BGMSlider;
-    public Slider SFXSlider;
+    [SerializeField] private Slider BGMSlider;
+    [SerializeField] private Slider SFXSlider;
 
     // 사운드 제어용 bool 
-    private bool _isMuted;
-    private bool _isSoundPlaying;
-    private bool _isSoundStoped;
+    private bool _isMuted; // 음소거  
+    private bool _isSoundPlaying; // 사운드 재생 
+    private bool _isSoundStoped;  // 사운드 멈춤 
 
-    private static SoundManager instance; 
+    // SingleTon
+    private static SoundManager instance = null;
     public static SoundManager Instance
     {
-        get { 
-            if (instance == null) 
-                instance = FindObjectOfType(typeof(SoundManager)) as SoundManager;  
-            return instance; 
-        } 
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType(typeof(SoundManager)) as SoundManager;
+            return instance;
+        }
     }
 
     private void Start()
     {
         // BGM 오디오 소스 
         BGMAudioSource = audioObject.GetComponent<AudioSource>();
-
+        BGMAudioSource.volume = BGMSlider.value; 
     }
-    public void SetBgmVolumeFromSlider(AudioSource audiosource, Slider soundSlider)
-    {
-        if (audiosource == null || soundSlider == null) return;
 
-        audiosource.volume = soundSlider.value;
+    // 배경음 셋팅 
+    public void SetBgmVolumeFromSlider(float volume)
+    {
+        if (BGMSlider == null || BGMAudioSource == null) return;
+
+        BGMAudioSource.volume = volume; 
+
         Debug.Log($"Slider 값에 따라 오디오 볼륨 변경");
     }
 
-    public void SetSoundClip(string clipName)
-    {
-         
-    }
-    public void ResetSoundClip()
-    {
-      
-    }
+    //public void SetSoundClip(string clipName, Vector3 audioPos, bool loop = false, float volume = 1.0f)
+    //{
 
+    //}
+    //public void ResetSoundClip()
+    //{
+
+    //}
+
+    public static implicit operator SoundManager(AudioPool v)
+    {
+        throw new NotImplementedException();
+    }
 }
