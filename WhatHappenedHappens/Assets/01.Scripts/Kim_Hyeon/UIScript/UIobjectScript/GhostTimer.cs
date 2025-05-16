b2cac71003d2d071187c18046ba77f9ce8d26a64
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-16 13:01:26 +0900

    #Add : 고스트 타이머 UI 용 스크립트 추가

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/UIobjectScript/GhostTimer.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/UIobjectScript/GhostTimer.cs
@@ -4,22 +4,19 @@ using UnityEngine.UI;
 
 public class GhostTimer : MonoBehaviour
 {
+
+    private ParadoxManager _paradoxManager;
+
     [Header("GhostTimerImages")] // 고스트 타이머 이미지 리스트 
-    public List<Image> ghostTimers = new List<Image>();
-    ParadoxManager _paradoxManager;
+    public List<Sprite> ghostTimers = new List<Sprite>(); // 변경할 스프라이트 리스트 
+    [SerializeField] private Image timerImage;            // 고스트 타이머 이미지    
 
     private void Start()
     {
         if (_paradoxManager == null) _paradoxManager = gameObject.AddComponent<ParadoxManager>();
         if (ghostTimers == null || ghostTimers.Count == 0)
             Debug.LogWarning("ghostTimer UI Image 리스트가 비어있습니다. 인스펙터에서 할당해주세요.");
-         
-        foreach (var timerImg in ghostTimers)
-        {
-            if (timerImg != null)
-                timerImg.gameObject.SetActive(true); 
-        }
-       
+
 
     }
 
@@ -30,8 +27,12 @@ public class GhostTimer : MonoBehaviour
     }
     // 남아있는 시간을 받아서, 남아있는 시간 동안 타이머 스프라이트가 변경. 
     // 남아있는 시간에 따라서 매초바다 변경 되어야 하기 때문에, Update에서 호출 필요
-    public void GhostTimerSpriteChanged(float timeRemaining)
+    public void GhostTimerSpriteChanged(float timeRemaining )
     {
+        if (timeRemaining < 0 || timeRemaining < 5f || ghostTimers.Count == 0) return;
+        Debug.LogWarning($"남아있는 시간을 초과했습니다. timeRemaining : {timeRemaining}");
+
+
 
     }
 }
