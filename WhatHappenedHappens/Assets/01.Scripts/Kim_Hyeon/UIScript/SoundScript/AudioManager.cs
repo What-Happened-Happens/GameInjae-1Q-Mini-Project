255098ffa933a5d8021f1bb86d14505b1296c06f
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-19 17:49:00 +0900

    # Fiexed  : 효과음과 배경음 음소거 오류 수정

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/AudioManager.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/AudioManager.cs
@@ -7,56 +7,98 @@ using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
-    [Header("Audio Mixer & Sliders")]
-    [SerializeField] protected AudioSource AudioSource;
+    [Header("Audio Source")]
+    [SerializeField] protected AudioSource _BGMaudioSource;
+    [SerializeField] protected AudioSource _SFXaudioSource;
     [SerializeField] protected GameObject AudioTarget;
-    [SerializeField] protected Slider AudioSlider;
-    [SerializeField] protected TMP_Text AudioValueText;
-        
-    public static AudioManager Instance { get; private set; }
+
+    [Header("Sliders")]
+    [SerializeField] protected Slider _BGMAudioSlider;
+    [SerializeField] protected Slider _SFXAudioSlider;
+
+    [Header("AudioValueText")]
+    [SerializeField] protected TMP_Text _BGMaudioText;
+    [SerializeField] protected TMP_Text _SFXaudioText;
+
+    [Header("SFX List")]
+    [SerializeField] private List<AudioSource> SFXsources = new List<AudioSource>();
+    private readonly Dictionary<AudioSource, float> _originalVolumes = new();
+
     protected bool _isMute = false;             // 사운드 플레이 중인지 확인하기 위한 변수 
 
     protected float _currentSliderValue;        // 현재 슬라이더 값 
     protected float _currentSourceVolume;       // 현재 오디오 소스 볼륨 
+
+    protected float _currentSFXSliderValue;     // 현재 슬라이더 값 
     protected float _PrevSoundValue;            // 이전 사운드 값 
-    public List<AudioSource> SFXsources { get; set; }
+    protected float _PrevSFXSoundValue;         // 이전 사운드 값 
+   
     protected async void Start()
     {
-        if (AudioSource == null)    Debug.LogError("AudioSource 할당되지 않았습니다.");
-        if (AudioSlider == null)    Debug.LogError("AudioSlider 할당되지 않았습니다.");
-        if (AudioValueText == null) Debug.LogError("VolumeText  할당되지 않았습니다.");
+        if (_BGMaudioSource == null)    Debug.LogError("AudioSource 할당되지 않았습니다.");
+        if (_BGMAudioSlider == null)    Debug.LogError("AudioSlider 할당되지 않았습니다.");
+        if (_BGMaudioText == null) Debug.LogError("VolumeText  할당되지 않았습니다.");
 
+        // BGM
         float saveCurrentAudio = await AudioLoad("save_CurrentSoundValue", 10f);
-        AudioSource.volume = saveCurrentAudio;
-        AudioSlider.value = saveCurrentAudio;
+        _BGMaudioSource.volume = saveCurrentAudio;
+        _BGMAudioSlider.value = saveCurrentAudio;
         _PrevSoundValue = saveCurrentAudio;
+
+        // SFX 
+        float saveCurrentSFXAudio = await AudioLoad("save_CurrentSFXaudio", 10f);
+        _SFXaudioSource.volume = saveCurrentSFXAudio;
+        _SFXAudioSlider.value = saveCurrentSFXAudio;
+        _PrevSFXSoundValue = saveCurrentSFXAudio; 
     }
 
     public async void OnClickMuteButton()  // 음소거 버튼을 눌렀을 때
     {
-        if (_isMute) return; 
+       if (_isMute)
+        {
+            await MuteAllAsync();
+            isMute(false);
+        }
+        else
+        {
+            await MuteAllAsync();
+            isMute(true);
+        }
+
+    }
+    public async Task MuteAllAsync()
+    {
+        if (_isMute) return;
 
         // 현재 볼륨을 저장해두고
-        _currentSourceVolume = AudioSource.volume;
+        _currentSourceVolume = _BGMaudioSource.volume;
 
         // 비동기 저장
         await AudioSave("save_CurrentSoundValue", _currentSourceVolume);
 
         isMute(false);
-       
+
         // 실제 음소거
-        AudioSource.volume = 0f;
-        AudioSlider.value = 0f;
-        Debug.Log($"음향을 음소거 시킵니다. 현 상태 : {_isMute}");
+        _BGMaudioSource.volume = 0f;
+        _BGMAudioSlider.value = 0f;
+        Debug.Log($"BGM 음향을 음소거 시킵니다. 현 상태 : {_isMute}");
 
-
-        //test code 
-        foreach (var audioSource in SFXsources)
+        //SFX
+        foreach (var src in SFXsources)
         {
-            audioSource.volume = 0f;
-        }
-    } 
+            if (src == null) continue;
+            await AudioSave("save_CurrentSFXaudio", src.volume);
+            _PrevSFXSoundValue = src.volume; 
+            _originalVolumes[_BGMaudioSource] = _PrevSFXSoundValue;
 
+            src.volume = 0f;
+            _SFXAudioSlider.value = 0f;
+            Debug.Log($"SFX 음향을 음소거 시킵니다. 현 상태 : {_isMute}");
+        }
+    }
+
+
+    // ------------------------------------------ 비동기 데이터 저장 및 로드 ------------------------------------//
     public async Task<float> AudioSave(string prefKey, float SaveData) // 슬라이더 변경점에 따라서 그 당시의 값을 저장 
     {
         await Task.Yield();
--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/AudioSliderUI.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/AudioSliderUI.cs
@@ -5,12 +5,17 @@ public class AudioSliderUI : AudioManager, IPointerDownHandler
 {
     private void OnEnable()
     {
-        AudioSlider.onValueChanged.AddListener(onAudioValueChanged);
-        onAudioValueChanged(AudioSlider.value);
+        // BGM
+        _BGMAudioSlider.onValueChanged.AddListener(onAudioValueChanged);
+        onAudioValueChanged(_BGMAudioSlider.value);
+        //SFX
+        _SFXAudioSlider.onValueChanged.AddListener(onAudioValueChanged);
+        onAudioValueChanged(_SFXAudioSlider.value);
+
     }
     private void OnDisable()
     {
-        AudioSlider.onValueChanged.RemoveListener(onAudioValueChanged);
+        _BGMAudioSlider.onValueChanged.RemoveListener(onAudioValueChanged);
     }
     public async void onAudioValueChanged(float value)
     {
@@ -20,13 +25,22 @@ public class AudioSliderUI : AudioManager, IPointerDownHandler
         {
             _currentSliderValue = value / 100f;
 
-            AudioSource.volume = _currentSliderValue;
-            AudioValueText.text = AudioSource.volume <= 0f ? "X" : $"{Mathf.RoundToInt(value)}%";
+            _BGMaudioSource.volume = _currentSliderValue;
+            _BGMaudioText.text = _BGMaudioSource.volume <= 0f ? "X" : $"{Mathf.RoundToInt(value)}%";
 
             //  이전 값에 현재 값을 로드 
             _PrevSoundValue = await AudioLoad("save_CurrentSoundValue", 0f);
             Debug.Log($"현재 음향 값을 다시 로드했습니다. ");
             Debug.Log($"음향을 다시 플레이 합니다.");
+
+            //SFX 
+            _SFXaudioSource.volume = _currentSliderValue;
+            _SFXaudioText.text = _SFXaudioSource.volume <= 0f ? "X" : $"{Mathf.RoundToInt(value)}%";
+
+            //  이전 값에 현재 값을 로드 
+            _PrevSFXSoundValue = await AudioLoad("save_CurrentSFXaudio", 0f);
+            Debug.Log($"현재 SFX음향 값을 다시 로드했습니다. ");
+            Debug.Log($"SFX 음향을 다시 플레이 합니다.");
 
             if (!_isMute && _currentSliderValue > 0f)
             {
@@ -37,10 +51,9 @@ public class AudioSliderUI : AudioManager, IPointerDownHandler
         else if (value == 0f)
         {
             isMute(false);
-            AudioSource.volume = 0f;
-            AudioSlider.value = 0f; 
-        }
-       
+            _BGMaudioSource.volume = 0f;
+            _BGMAudioSlider.value = 0f; 
+        }      
 
     }
 
