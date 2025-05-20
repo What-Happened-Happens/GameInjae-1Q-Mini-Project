8e9b7829697ae071179c48ebd49ca8047ebd9b33
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-20 22:41:02 +0900

    .

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/SFXAudioPlay.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/SoundScript/SFXAudioPlay.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class SFXAudioPlay : SFXAudioManager
 {
+    [Header("AudioSource")]
+    [SerializeField] private AudioSource audioSource;
+
+    [Header("Short or Long Threshold")]
+    [SerializeField] private float shortThreshold = 0.5f; 
+
     public void ClickEventAudioPlay(float MouseInput)
     {
 
