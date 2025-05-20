092894cc81ad1b72f56a7cc18e93cd97a7bf6366
Popimuty <bin9825aaa@gmail.com>
2025-05-20 12:01:24 +0900

    0520 오전
    카메라 업데이트 위치 수정

---
+++ WhatHappenedHappens/Assets/01.Scripts/Scripts_SeongBin/OverlayCameraSize.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Overlays;
+using UnityEngine;
+
+public class OverlayCameraSize : MonoBehaviour
+{
+    public PixelPerfectZoomCinemachine virCamerasize;
+    Camera CameraTmp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraTmp = GetComponent<Camera>();
+        CameraTmp.orthographicSize = virCamerasize.defaultCameraSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraTmp.orthographicSize = virCamerasize.currentZoom;
+    }
+}
