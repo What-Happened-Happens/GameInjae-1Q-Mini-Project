63412ffa17735883e4432f7af185ad0fdbfecc1f
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2025-05-22 18:22:40 +0900

    🔨 Feat | MenuUI 제작

--- WhatHappenedHappens/Assets/01.Scripts/Moon/Stage4Manager.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Moon/Stage4Manager.cs
@@ -4,14 +4,75 @@ using UnityEngine;
 
 public class Stage4Manager : MonoBehaviour
 {
+    public GameObject AudioSettingUI;
+    public GameObject MenuUI;
+
+    public GameObject pauseScreen;
+
     void Start()
     {
         // 첫번째 클립 재생 
         SoundManager.Instance.PlayBGM(SoundManager.Instance.bgmClips[0]);
+
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape) && !MenuUI.activeSelf)
+        {
+            MenuUI.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && MenuUI.activeSelf)
+        {
+            AudioSettingUI.SetActive(false);
+            MenuUI.SetActive(false);
+        }
+    }
+
+    public void ContinuePlay()
+    {
+        SoundManager.Instance.UISFX("UI_Click", 1f);
+
+        AudioSettingUI.SetActive(false);
+        MenuUI.SetActive(false);
+
+        // pause 해제하기 
+        pauseScreen.GetComponent<pauseScreen>().isScreenPause = false;
+    }
+
+    public void NewGame()
+    {
+        SoundManager.Instance.UISFX("UI_Click", 1f);
+        // 현재 씬 새로 시작 로직
+        SceneController.Instance.ReloadCurrentScene();
+    }
+
+    public void LoadOptions()
+    {
+        SoundManager.Instance.UISFX("UI_Click", 1f);
+
+        if (AudioSettingUI.activeSelf)
+        {
+            AudioSettingUI.SetActive(false);
+        }
+        else
+        {
+            AudioSettingUI.SetActive(true);
+        }
+    }
+
+    public void GotoStart()
+    {
+        SoundManager.Instance.UISFX("UI_Click", 1f);
+        SceneController.Instance.LoadScene("StartScene");
+    }
+
+    public void QuitGame()
+    {
+        SoundManager.Instance.UISFX("UI_Click", 1f);
+
+        // 게임 종료 로직
+        Application.Quit();
+        Debug.Log("게임 종료");
     }
 }
