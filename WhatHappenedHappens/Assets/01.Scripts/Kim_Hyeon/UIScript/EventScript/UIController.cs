324e14506533ab993da72425d78f3986373a4ee3
Sanao2 <SanaoAiza1102@gmail.com>
2025-05-22 18:03:48 +0900

    # 오브젝트 아이콘 출력 오류 해결

--- WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/EventScript/UIController.cs
+++ WhatHappenedHappens/Assets/01.Scripts/Kim_Hyeon/UIScript/EventScript/UIController.cs
@@ -8,31 +8,37 @@ public class UIController : MonoBehaviour
     public GameObject _player; // 플레이어 타겟 
     public GameObject _target; // 오브젝트 타겟 
 
-    [Header("Icons")]
+    [Header("Use Icons")] // 나타나게 할 아이콘 
     public List<GameObject> _icons = new List<GameObject>(); // 아이콘 리스트 
+
+    [Header("Use TargetIcon")] // 나타나게 할 아이콘 
+    public GameObject _targetIcon;
+    public GameObject _targetIcon_Move;
 
     [Header("Distance Setting")]
     private float distance = 0f; // 플레이어와 타겟 사이 거리 
     private float minDistance = 2f; // 최소 거리 
     private float maxDistance = 3f; // 최대 거리 
     private float offset = 2f;
-    private GameObject currentIcon; // 현재 아이콘
-
-    [Header("Use TargetIcon")]
-    public GameObject _targetIcon; // 나타나게 할 아이콘 
-
+    private bool isStageClear = true; // 스테이지 클리어 여부 > Test 
 
     private void Awake()
     {
         foreach (var icon in _icons)
         {
+            if (icon.name == _targetIcon_Move.name)
+            {
+                icon.SetActive(true); // 이동 아이콘은 활성화 
+            }// 이동 아이콘은 제외
             icon.gameObject.SetActive(false); // 아이콘 비활성화 
         }
+
     }
 
     private void Update()
     {
-        distance = Vector2.Distance(_player.transform.position, _target.transform.position); // 거리 계산 
+        if (_player == null || _target == null) return;
+        distance = Vector3.Distance(_player.transform.position, _target.transform.position); // 거리 계산 
 
         Debug.Log($"Update : 플레이어와 타겟의 거리:  {distance}");
         IconUpdate();
@@ -40,52 +46,59 @@ public class UIController : MonoBehaviour
 
     public void IconUpdate()
     {
-        if (distance < minDistance )
+
+        if (distance < minDistance)
         {
-            Debug.Log($"Show : 플레이어와 타겟의 거리:  {distance}"); 
-            IconShow(_targetIcon); // 아이콘 활성화 
+            Debug.Log($"Show : 플레이어와 타겟의 거리:  {distance}");
+            IconShow(_targetIcon); // 아이콘 활성화
+            MoveIconAll(_targetIcon_Move, true); // 이동 아이콘 활성화 
+
         }
-        else if(distance > maxDistance) 
+        else if (distance > maxDistance)
         {
             Debug.Log($"Hide : 플레이어와 타겟의 거리:  {distance}");
-            HideIcon(); // 아이콘 비활성화 
+            HideIcon(_targetIcon); // 아이콘 비활성화 
+
         }
+        else if (isStageClear)
+        {
+            MoveIconAll(_targetIcon_Move, false); // 이동 아이콘 비활성화 
+        }
+        else return; 
 
     }
 
-    public void IconShow(GameObject iconObj)
+    private void IconShow(GameObject iconObj)
     {
-        if (currentIcon == iconObj && iconObj.activeSelf)
-        {
-            UpdatePosition(iconObj); // 아이콘 위치 업데이트 
-            return; // 이미 활성화된 아이콘이면 아무것도 하지 않음
-        }
-
-        foreach (var go in _icons)
-        {
-            go.SetActive(false); // 모든 아이콘 비활성화 
-        }
-
-        iconObj.SetActive(true); // 선택한 아이콘 활성화 
-        currentIcon = iconObj; // 현재 아이콘 설정 
-
-        UpdatePosition(iconObj); // 아이콘 위치 업데이트 
+        if (!iconObj.activeSelf)
+            iconObj.SetActive(true);
+        UpdatePosition(iconObj);
     }
 
-    private void HideIcon()
+    // 기본 아이콘 끄기
+    private void HideIcon(GameObject iconObj)
     {
-        if (currentIcon != null)
+        if (iconObj != null && iconObj.activeSelf)
+            iconObj.SetActive(false);
+    }
+    private void MoveIconAll(GameObject iconObj, bool isActive) // isActive : 이동 아이콘 활성화 여부 // 숨기고 싶을 땐 따로 시간에 맞춰서 false 로 바꾸기 
+    {
+        iconObj.SetActive(isActive); // 이동 아이콘 활성화 
+        foreach (GameObject child in iconObj.GetComponentsInChildren<GameObject>())
         {
-            currentIcon.SetActive(false); // 현재 아이콘 비활성화 
-            currentIcon = null; // 현재 아이콘 초기화 
+            child.SetActive(isActive); // 자식 오브젝트 포함 
+            child.transform.position = _player.transform.position + Vector3.up;  // 아이콘 위치 업데이트
         }
     }
+
+    // 이동 아이콘 끄기
+  
 
     private void UpdatePosition(GameObject iconObj)
     {
-        Vector3 IconPos = _target.transform.position + Vector3.up * offset; // 아이콘 위치 설정 
+        
+            iconObj.transform.position = _target.transform.position + Vector3.up * offset;  // 아이콘 위치 업데이트
 
-        iconObj.transform.position = IconPos; // 아이콘 위치 업데이트
     }
 
 }
